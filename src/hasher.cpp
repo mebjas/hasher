95b8ce4a1408db1109cee6386dd26f628e3bae87
minhaz <minhazav@gmail.com>
2014-09-22 02:53:09 +0530

    base64 encode system -> openssl

--- src/hasher.cpp
+++ src/hasher.cpp
@@ -5,12 +5,20 @@
 #include <math.h>
 #include <cstdlib>
 
+// openssl libraries
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+
 using namespace std;
 
+// Global definations
 #define MOD 1000000007
 #define rec(i, n) for(int i = 0; i < n; i++)
 #define max(a,b) ((a)>(b))?(a):(b)
 #define u unsigned
+#define NEWLINE printf("\n")
+
+// Global functions
 
 /**
  * Function to print all the help message
@@ -24,7 +32,6 @@ void help() {
 	printf("\thasher -b64 <string> : to calculate both base64 encoded & decoded string \n");
 	printf("\thasher -b64E <string> : to calculate base64 encoded string \n");
 	printf("\thasher -b64D <string> : to calculate base64 decoded string \n");
-
 
 	// -- examples
 	printf("\tExample: hasher -md5 hector\n");
@@ -42,16 +49,18 @@ void md5(char s[]) {
 	system(command);
 }
 
-/**
- * Fucntion to calculate and print the base64 encoded string
- *
- * @todo - later shift this to use openssl library rather than running
- * system command ...
- */
+// Fucntion to calculate and print the base64 encoded string
 void base64Encode(char s[]) {
-	char command[100];
-	int x = sprintf(command, "echo \"%s\" | base64", s);
-	system(command);
+	printf("base64 encode: ");
+
+	BIO *bio, *b64;
+	b64 = BIO_new(BIO_f_base64());
+	bio = BIO_new_fp(stdout, BIO_NOCLOSE);
+	BIO_push(b64, bio);
+	BIO_write(b64, s, strlen(s));
+	BIO_flush(b64);
+	BIO_free_all(b64);
+	NEWLINE;
 }
 
 /**
@@ -63,7 +72,9 @@ void base64Encode(char s[]) {
 void base64Decode(char s[]) {
 	char command[100];
 	int x = sprintf(command, "echo \"%s\" | base64 -D", s);
+	printf("base64 decode: ");
 	system(command);
+	NEWLINE;
 }
 
 // ---- Declare Global Variables from here
@@ -84,12 +95,9 @@ int main(int argc, char *argv[]) {
 		// For md5 digest
 		md5(argv[2]);
 	} else if (!strcmp(argv[1],"-b64")) {
-		// For both base64 encode & decode
-		// Encode
-		printf("-- base64 encode --\n");
-		base64Encode(argv[2]);
 
-		printf("-- base64 decode --\n");
+		// For both base64 encode & decode
+		base64Encode(argv[2]);
 		base64Decode(argv[2]);
 
 	} else if (!strcmp(argv[1],"-b64E")) {
@@ -102,13 +110,8 @@ int main(int argc, char *argv[]) {
 
 	} else if (!strcmp(argv[1],"-a")) {
 		// -- call each functions one by one!
-		printf("-- MD5 --\n");
 		md5(argv[2]);
-
-		printf("-- base64 encode -- \n");
 		base64Encode(argv[2]);
-
-		printf("-- base64 decode --\n");
 		base64Decode(argv[2]);
 
 	}
